c0478af4969c6b63cee56715eeb8f34c13a126e8
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-15 22:00:25 +0200

    Stuff the main package, now processing cmd args, and launching the daemon. It's taking shape

--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+
+	"github.com/LeReverandNox/GuessWhat/src/daemon"
+)
+
+func parseArgs() *daemon.Config {
+	cfg := &daemon.Config{}
+
+	flag.StringVar(&cfg.Host, "host", "0.0.0.0", "The host to bind to")
+	flag.StringVar(&cfg.Port, "port", "3000", "The port to bind to")
+
+	flag.Parse()
+	return cfg
+}
 
 func main() {
-	fmt.Println("Hello, World !")
+	cfg := parseArgs()
+
+	err := daemon.Run(cfg)
+	if err != nil {
+		log.Fatalf("Something went wrong during launch : %v", err)
+	}
 }
