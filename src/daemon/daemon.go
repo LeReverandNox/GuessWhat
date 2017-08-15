af8ce9ec8eaba32c8a81f397c5109abc5fd1c0ee
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-15 21:59:56 +0200

    Add a daemon package, used to launch the http server

---
+++ src/daemon/daemon.go
@@ -0,0 +1,29 @@
+package daemon
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/LeReverandNox/GuessWhat/src/router"
+)
+
+// Config is a structure holding the daemon's config
+type Config struct {
+	Host string
+	Port string
+}
+
+// Run is the method that launch the server
+func Run(cfg *Config) error {
+	router := router.NewRouter()
+
+	fmt.Printf("The Guess What app is now running on %v:%v", cfg.Host, cfg.Port)
+
+	err := http.ListenAndServe(cfg.Host+":"+cfg.Port, router)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
