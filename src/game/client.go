0724e36973692b36dcb2969301a24d73e7c0b337
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-17 22:43:34 +0200

    Add methods to client, to manage the clients

--- src/game/client.go
+++ src/game/client.go
@@ -1,8 +1,24 @@
 package game
 
-import "github.com/LeReverandNox/GuessWhat/src/socket"
+import (
+	"github.com/LeReverandNox/GuessWhat/src/socket"
+	"golang.org/x/net/websocket"
+)
 
 type Client struct {
 	Socket   *socket.Socket
 	Nickname string
+}
+
+func NewClient(ws *websocket.Conn) *Client {
+	client := Client{}
+	client.Socket = &socket.Socket{ws}
+	return &client
+}
+
+func (game *Game) AddClient(ws *websocket.Conn) *Client {
+	client := NewClient(ws)
+	game.Clients = append(game.Clients, client)
+	return client
+
 }
