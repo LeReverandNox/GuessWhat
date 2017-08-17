7b1dce876ba52697458509f387115c96771da892
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-18 01:05:32 +0200

    Game now have a Messages fields, for the chat history

--- src/game/game.go
+++ src/game/game.go
@@ -1,14 +1,16 @@
 package game
 
 type Game struct {
-	Clients []*Client
-	Rooms   []*Room
+	Clients  []*Client
+	Rooms    []*Room
+	Messages []*Message
 }
 
 func NewGame() *Game {
 	game := Game{}
 	game.Clients = make([]*Client, 0)
 	game.Rooms = make([]*Room, 0)
+	game.Messages = make([]*Message, 0)
 
 	return &game
 }
