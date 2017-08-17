07f6408deadc46e5c62fc11989546d1f52ce8da5
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-17 22:42:50 +0200

    Move rooms method in room.go, instead of game.go

--- src/game/game.go
+++ src/game/game.go
@@ -1,7 +1,5 @@
 package game
 
-import "errors"
-
 type Game struct {
 	Clients []*Client
 	Rooms   []*Room
@@ -13,23 +11,4 @@ func NewGame() *Game {
 	game.Rooms = make([]*Room, 0)
 
 	return &game
-}
-
-// AddRoom adds a room to the server
-func (game *Game) AddRoom(name string) error {
-	if !game.isRoomExisting(name) {
-		room := NewRoom(name)
-		game.Rooms = append(game.Rooms, room)
-		return nil
-	}
-	return errors.New("The channel " + name + " already exist")
-}
-
-func (game *Game) isRoomExisting(name string) bool {
-	for _, room := range game.Rooms {
-		if room.Name == name {
-			return true
-		}
-	}
-	return false
 }
--- src/game/room.go
+++ src/game/room.go
@@ -1,5 +1,7 @@
 package game
 
+import "errors"
+
 type Room struct {
 	Name    string
 	Clients []*Client
@@ -13,4 +15,23 @@ func NewRoom(name string) *Room {
 	room.Clients = make([]*Client, 0)
 
 	return &room
+}
+
+// AddRoom adds a room to the server
+func (game *Game) AddRoom(name string) error {
+	if !game.isRoomExisting(name) {
+		room := NewRoom(name)
+		game.Rooms = append(game.Rooms, room)
+		return nil
+	}
+	return errors.New("The channel " + name + " already exist")
+}
+
+func (game *Game) isRoomExisting(name string) bool {
+	for _, room := range game.Rooms {
+		if room.Name == name {
+			return true
+		}
+	}
+	return false
 }
