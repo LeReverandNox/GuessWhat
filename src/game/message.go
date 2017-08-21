ba0a22159a0acdbacbb73f884aa350f89a275daf
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-21 18:42:25 +0200

    Remove game methods from Message, and mod the struct to sture a *Client instead of a nickname

--- src/game/message.go
+++ src/game/message.go
@@ -1,33 +1,18 @@
 package game
 
 import "time"
-import "log"
 
 type Message struct {
-	Nickname string
-	Content  string
-	Date     time.Time
+	Sender  *Client
+	Content string
+	Date    time.Time
 }
 
-func NewMessage(nickname string, content string, date time.Time) *Message {
+func NewMessage(sender *Client, content string, date time.Time) *Message {
 	message := Message{}
-	message.Nickname = nickname
+	message.Sender = sender
 	message.Content = content
 	message.Date = date
 
 	return &message
-}
-
-func (game *Game) AddMessage(nickname string, content string) *Message {
-	message := NewMessage(nickname, content, time.Now())
-	game.Messages = append(game.Messages, message)
-
-	return message
-}
-
-func (game *Game) ListMessages() {
-	log.Print("Voici les messages du serveur")
-	for _, msg := range game.Messages {
-		log.Print(msg)
-	}
 }
