ee25a2d7fcb7f7c3acab14c61aa00fb51cfd165e
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-21 19:22:23 +0200

    Add a Messages attr in Room, so each Room can have is own chat

--- src/game/room.go
+++ src/game/room.go
@@ -3,13 +3,15 @@ package game
 import (
 	"errors"
 	"log"
+	"time"
 )
 
 type Room struct {
-	Name    string
-	Clients []*Client
-	Drawer  string
-	Image   string
+	Name     string
+	Messages []*Message
+	Clients  []*Client
+	Drawer   string
+	Image    string
 }
 
 // NewRoom creates a new room and returns it
@@ -17,6 +19,7 @@ func NewRoom(name string) *Room {
 	room := Room{}
 	room.Name = name
 	room.Clients = make([]*Client, 0)
+	room.Messages = make([]*Message, 0)
 
 	return &room
 }
@@ -41,6 +44,7 @@ func (room *Room) AddClient(client *Client) error {
 	return errors.New("The client " + client.Nickname + " is already in the room " + room.Name)
 }
 
+// IsClientIn returns true if the given client is in.
 func (room *Room) IsClientIn(clientToSearch *Client) bool {
 	for _, client := range room.Clients {
 		if clientToSearch == client {
@@ -48,6 +52,14 @@ func (room *Room) IsClientIn(clientToSearch *Client) bool {
 		}
 	}
 	return false
+}
+
+// AddMessage adds a message to the game
+func (room *Room) AddMessage(sender *Client, content string) *Message {
+	message := NewMessage(sender, content, time.Now())
+	room.Messages = append(room.Messages, message)
+
+	return message
 }
 
 // ListClients lists the clients of the room
